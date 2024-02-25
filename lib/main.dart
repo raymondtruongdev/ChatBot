10e60226fa1baa13a1b520edbe96ca1343c84424
raymond <raymondtruongdev@gmail.com>
2024-02-25 19:32:55 +0700

    fix multiple build MyApp widget when keyboard are showing => fix it by add HomePage widget.  Turn off fullscreen mode ( when it is on, it will cause 2 build times when the app start )

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:chat_bot/controller/chatbot_controller.dart';
 import 'package:chat_bot/logger_custom.dart';
 import 'package:chat_bot/pages/chat_page.dart';
 import 'package:chat_bot/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final ChatBotController chatBotController =
+    Get.put(ChatBotController(), permanent: true);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,35 +21,47 @@ class MyApp extends StatelessWidget {
     // Set immersive system UI mode for Android (full screen)
     //- However it will make 2 times init app
     // --> can not fix this issue now
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
     CustomLogger logger = CustomLogger();
-    // Get Size of device
-    double widthScreenDevice = MediaQuery.of(context).size.width;
-    double heightScreenDevice = MediaQuery.of(context).size.height;
-    if ((widthScreenDevice > 0) && (heightScreenDevice > 0)) {
-      logger.info('Width: $widthScreenDevice, Height: $heightScreenDevice');
-    } else {
-      logger.error('Screen size is 0');
-    }
+    logger.error('Screen 1 build');
 
-    if (widthScreenDevice == 0 || heightScreenDevice == 0) {
-      return const MaterialApp(
-        home: Scaffold(body: Center(child: Text("Loading..."))),
-        debugShowCheckedModeBanner: false,
-      );
-    } else {
-      return MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false, // turn off debug banner
         theme: lighMode,
         home: SafeArea(
           child: Scaffold(
               backgroundColor: Theme.of(context).colorScheme.background,
               body: const Center(
-                child: ChatPage(),
+                child: HomePage(),
               )),
-        ),
-      );
+        ));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    CustomLogger logger = CustomLogger();
+    // Get Size of device
+    double widthScreenDevice = MediaQuery.of(context).size.width;
+    double heightScreenDevice = MediaQuery.of(context).size.height;
+    if ((widthScreenDevice > 0) && (heightScreenDevice > 0)) {
+      logger.debug('Width: $widthScreenDevice, Height: $heightScreenDevice');
+    } else {
+      logger.error('Screen size is 0');
+    }
+    if ((widthScreenDevice > 0) && (heightScreenDevice > 0)) {
+      return const ChatPage();
+    } else {
+      return const Center(child: Text('Loading......'));
     }
   }
 }
