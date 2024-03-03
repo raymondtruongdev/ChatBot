0f789bf647527322524349ebe6916baa29f0fd33
raymond <raymondtruongdev@gmail.com>
2024-03-03 14:08:49 +0700

    add demo auto speech to text

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:chat_bot/controller/chatbot_controller.dart';
 import 'package:chat_bot/controller/voice_to_text_controller.dart';
 import 'package:chat_bot/logger_custom.dart';
@@ -8,6 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
@@ -40,24 +42,99 @@ class MyApp extends StatelessWidget {
           child: Scaffold(
               backgroundColor: Theme.of(context).colorScheme.background,
               body: const Center(
-                child: HomePage(),
+                child: SpeechScreen(),
               )),
         ));
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class SpeechScreen extends StatefulWidget {
+  const SpeechScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SpeechScreen> createState() => _SpeechScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SpeechScreenState extends State<SpeechScreen> {
+  late stt.SpeechToText _speech;
+  late bool _isListening = false;
+  late String _text = 'Press the button and start speaking';
+  late double _confidence = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+              child: Text(
+                  'Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'))),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Colors.green,
+        duration: const Duration(milliseconds: 2000),
+        startDelay: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _isListen,
+          backgroundColor: Colors.blue,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: Text(
+            _text,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  void _isListen() async {
+    _text = '';
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) {
+          CustomLogger().debug('onStatus: $val');
+          if (val == 'done') {
+            _speech.stop();
+            setState(() => _isListening = false);
+            _speech.stop();
+            CustomLogger().debug('Speech stopped');
+          }
+        },
+        onError: (val) => CustomLogger().error('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            CustomLogger().info(_text);
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      _speech.stop();
+      setState(() => _isListening = false);
+      _speech.stop();
+      CustomLogger().debug('Speech stopped');
+    }
   }
 }
